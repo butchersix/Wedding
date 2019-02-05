80ed63774349de275e11483a5ca01449402e4a87
butchersix <jasperhavenbriones@gmail.com>
2019-02-05 13:22:09 +0800

    [Added] Different pages (already linked)

--- platforms/browser/www/js/my-app.js
+++ platforms/browser/www/js/my-app.js
@@ -7,24 +7,52 @@ var app = new Framework7({
     id: 'com.wedding.project',
     // Enable swipe panel
     panel: {
-      swipe: 'left',
+        swipe: 'left',
     },
     // Add default routes
     routes: [
-      {
-        path: '/about/',
-        url: 'about.html',
-      }
+        {
+            path: '/home/',
+            url: 'index.html',
+        },
+        {
+            path: '/bride-and-groom/',
+            url: 'bride-and-groom.html',
+        },
+        {
+            path: '/wedding-party/',
+            url: 'wedding-party.html',
+        },
+        {
+            path: '/events/',
+            url: 'events.html',
+        },
+        {
+            path: '/attire/',
+            url: 'attire.html',
+        },
+        {
+            path: '/location/',
+            url: 'location.html',
+        },
+        {
+            path: '/food-menu/',
+            url: 'food-menu.html',
+        }
     ],
     // ... other parameters
-  });
-  
-  var mainView = app.views.create('.view-main');
-  
-  // App Events
-  app.on('panelClose', function (panel) {
+});
+
+var mainView = app.views.create('.view-main');
+
+// App Events
+app.on('panelClose', function (panel) {
     console.log('Panel ' + panel.side + ': close');
-  });
-  app.on('panelClosed', function (panel) {
+});
+app.on('panelClosed', function (panel) {
     console.log('Panel ' + panel.side + ': closed');
-  });
+});
+
+// $('.sidebar-white a').click(function() {
+//     $('.title-text').html($(this).html());
+// });
